fb9f3f271e3f48d18874c1f6b3500b4402d108eb
ahmfarisi <ahmfarisi@gmail.com>
2022-12-01 13:09:51 +0700

    Create Model and Create Packages

--- app/src/main/java/com/ahmfarisi/justquote/Activity/MainActivity.java
+++ app/src/main/java/com/ahmfarisi/justquote/Activity/MainActivity.java
@@ -1,8 +1,10 @@
-package com.ahmfarisi.justquote;
+package com.ahmfarisi.justquote.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+
+import com.ahmfarisi.justquote.R;
 
 public class MainActivity extends AppCompatActivity {
 
