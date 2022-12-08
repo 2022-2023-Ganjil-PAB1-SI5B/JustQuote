e80998604c15e01206ef47a5f5e442a9326dd4f3
ahmfarisi <ahmfarisi@gmail.com>
2022-12-08 13:12:43 +0700

    Update MainActivity.java

--- app/src/main/java/com/ahmfarisi/justquote/Activity/MainActivity.java
+++ app/src/main/java/com/ahmfarisi/justquote/Activity/MainActivity.java
@@ -1,16 +1,24 @@
 package com.ahmfarisi.justquote.Activity;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
+import android.widget.ProgressBar;
 
 import com.ahmfarisi.justquote.R;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView rvQuote;
+    private ProgressBar pbQuote;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        rvQuote = findViewById(R.id.rv_quote);
+        pbQuote = findViewById(R.id.pb_quote);
+        rvQuote.setLayoutManager(new LinearLayoutManager(this));
     }
 }
